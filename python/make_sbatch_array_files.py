ed4048471e3dc297c828be9cd08a258cda0db460
rfinn <rfinn>
2022-05-24 20:35:05 -0400

    program to make array scripts

--- python/make_sbatch_array_files.py
+++ python/make_sbatch_array_files.py
@@ -47,11 +47,10 @@ def write_output(script_id,input_file,submit=False):
     output += "module load gnu9\n"
     output += "\n"
     output += "# Move to the directory needed - defaults to the submission directory\n"
-    output += "cd $HOME/research/Virgo/magphysParallel/output/\n"
     output += "\n"
     output += "# perform calculation\n"
     output += "\n"
-    output += f'LINE=$(sed -n "$SLURM_ARRAY_TASK_ID"p {input_file})\n'    
+    output += f'LINE=$(sed -n "$SLURM_ARRAY_TASK_ID"p $HOME/research/Virgo/magphysParallel/output/{input_file})\n'    
     output += f"python {HOME}/github/virgoseds/python/run1magphys.py $LINE\n"
 
     outfname = f"JOB_{script_id}.sh"
