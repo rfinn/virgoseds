10d8fba6c3a004c8a8858b6bccbd27c0b1f8028f
rfinn <rfinn>
2024-05-21 19:07:33 -0400

    new code to calculate percentiles from magphys output rather than parsing the percentile line reported in *.fit files.

--- python/gatherMagphys.py
+++ python/gatherMagphys.py
@@ -40,15 +40,66 @@ def parse_percentile(line):
     return float(t[2]),float(t[1]),float(t[3])
 
 
+def calc_percentiles(lines,plotflag=False):
+    '''sum total percentile, calc and return med and 68% conf interval  '''
+    from scipy.interpolate import interp1d
+    
+    val = []
+    prob = []
+    for l in lines:
+        t = l.split()
+        val.append(float(t[0]))
+        prob.append(float(t[1]))        
+        #try:
+        #    val.append(float(t[0]))
+        #    prob.append(float(t[0]))
+        #except ValueError:
+        #    print("error splitting line: ",l)
+        #    sys.exit()
+
+    val = np.array(val,'d')
+    prob = np.array(prob,'d')
+    
+    # calculate the total probability
+    ptotal = np.sum(prob)
+
+    # calculate cumulative sum and create inter function
+    integral2 = np.cumsum(prob)
+    interp_profile = interp1d(integral2,val)
+    
+    # 0.025,0.16,0.50,0.84,0.975/
+    thresholds = [0.16,0.5,0.84]
+    percentiles = []
+    for p in thresholds:
+        try:
+            t = interp_profile(p*integral2[-1])
+            percentiles.append(float(f"{t:.3f}"))
+        except ValueError:
+            percentiles.append(np.nan)
+
+    if plotflag:
+        plt.figure()
+        plt.subplot(1,2,1)
+        plt.plot(val,prob,'b.')
+        plt.subplot(1,2,2)
+        plt.plot(val,integral2)
+        for p in percentiles:
+            plt.axvline(x=p,ls='--')
+    return percentiles[1], percentiles[0], percentiles[2]
+    
+
+
 ###################################################################
 #### SET UP ARGPARSE
 ###################################################################
 
 parser = argparse.ArgumentParser(description ='Gather output from magphys. Write output table to ~/research/Virgo/tables-north/v2/')
 parser.add_argument('--plot',dest = 'plot', default=False,action='store_true',help='make plots of SED and pdf histograms.  This increases the execution time A LOT!!!  Default is false.')
+parser.add_argument('--verbose',dest = 'verbose', default=False,action='store_true',help='set this to print extra statements')
 
 parser.add_argument('--magdir',dest = 'magdir', default='research/Virgo/magphys/magphysParallelGrawp/output/',help='directory to grab the magphys results from.  the default is HOMEDIR+research/Virgo/maphys/magphysParallelGrawp/output/')
 parser.add_argument('--id',dest = 'id', default=None, help='identifier for output table, like nozband or legacyExt or salimExt')
+
 
     
 args = parser.parse_args()
@@ -155,18 +206,28 @@ tau_V_ISM_percent = []
 Mdust_percent = []
 
             
+# save the observed fluxes and errors
+obs_flux = []
+obs_flux_err = []
+best_flux = []
 
 
 
 nmagphys = 0
 for d in dirlist:
+    
     if d.startswith('job'):
         continue
-    #print('checking directory ',d)
+    if not os.path.isdir(d):
+        continue
+    if args.verbose:
+        print('checking directory ',d)
+        print(os.getcwd())
+    
     sedfile = d+'/'+d+'.sed'
     fitfile = d+'/'+d+'.fit'
     #print(d)
-    if os.path.exists(sedfile):
+    if os.path.exists(fitfile):
         #print('\t found results for ',d)
         lastdir = d
         os.chdir(d)
@@ -207,6 +268,10 @@ for d in dirlist:
         flux = np.array(fit_lines[2].split(),'d') # observed L
         e_flux = np.array(fit_lines[3].split(),'d') # error
 
+        # keep all fluxes
+        obs_flux.append(flux)
+        obs_flux_err.append(e_flux)
+
         # this is the output from the best-fit model
         # not sure exactly what these are yet, except for z of course...
         i_sfh,i_ir,chi2,z = np.array(fit_lines[8].split(),'d')
@@ -214,7 +279,10 @@ for d in dirlist:
             xi_Ctot,xi_PAHtot,xi_MIRtot,xi_Wtot,\
             tvism,Mdust,SFR = np.array(fit_lines[10].split(),'d')
 
+        # best flux
+        best_flux.append(np.array(fit_lines[12].split(),'d')) # best L
 
+        
         # get sfr and mstars
         sfr_best.append(np.log10(SFR))
         mstar_best.append(np.log10(mstar))
@@ -250,94 +318,114 @@ for d in dirlist:
         # leaving all parameters here in case we want to add additional output
         
         #fmu_SFR = parse_pdf(fit_lines[16:36])
-        t = parse_percentile(fit_lines[37])
+        #t = parse_percentile(fit_lines[37])
+        t = calc_percentiles(fit_lines[16:36])
         fmuSFR_med.append(float(t[0]))        
         fmuSFR_percent.append(np.array(t[1:]))
         
         #fmu_IR = parse_pdf(fit_lines[39:59])
-        t = parse_percentile(fit_lines[60])
+        #t = parse_percentile(fit_lines[60])
+        t = calc_percentiles(fit_lines[39:59])        
         fmuIR_med.append(float(t[0]))        
         fmuIR_percent.append(np.array(t[1:]))
 
         #mu = parse_pdf(fit_lines[62:82])
         t = parse_percentile(fit_lines[83])
+        t = calc_percentiles(fit_lines[62:82])        
         mu_med.append(float(t[0]))        
         mu_percent.append(np.array(t[1:]))
         
         #tau_V = parse_pdf(fit_lines[85:133])
-        t = parse_percentile(fit_lines[134])
+        #t = parse_percentile(fit_lines[134])
+        t = calc_percentiles(fit_lines[85:133])        
         tau_V_med.append(float(t[0]))        
         tau_V_percent.append(np.array(t[1:]))
 
         #sSFR = parse_pdf(fit_lines[136:206])
-        t = parse_percentile(fit_lines[207])
+        #t = parse_percentile(fit_lines[207])
+        t = calc_percentiles(fit_lines[136:206])        
         ssfr_med.append(float(t[0]))
         ssfr_percent.append(np.array(t[1:]))
 
         #Mstar = parse_pdf(fit_lines[209:269])
         #t = parse_percentile(fit_lines[270])
-        t = parse_percentile(fit_lines[310])                
+        #t = parse_percentile(fit_lines[310])
+        t = calc_percentiles(fit_lines[209:309])
         mstar_med.append(float(t[0]))
         mstar_percent.append(np.array(t[1:]))
 
         #Ld_tot = parse_pdf(fit_lines[272:332])
         #t = parse_percentile(fit_lines[333])
-        t = parse_percentile(fit_lines[413])
+        #t = parse_percentile(fit_lines[413])
+        t = calc_percentiles(fit_lines[312:412])
         Ld_tot_med.append(float(t[0]))
         Ld_tot_percent.append(np.array(t[1:]))
         
         #Tc_ISM = parse_pdf(fit_lines[335:345])
         #t = parse_percentile(fit_lines[346])
-        t = parse_percentile(fit_lines[426])
+        #t = parse_percentile(fit_lines[426])
+        t = calc_percentiles(fit_lines[415:425])        
         Tc_ISM_med.append(float(t[0]))
         Tc_ISM_percent.append(np.array(t[1:]))
 
         
         #Tw_BC = parse_pdf(fit_lines[348:378])
         #t = parse_percentile(fit_lines[379])
-        t = parse_percentile(fit_lines[459])
+        #t = parse_percentile(fit_lines[459])
+        t = calc_percentiles(fit_lines[428:458])        
         Tw_BC_med.append(float(t[0]))
         Tw_BC_percent.append(np.array(t[1:]))
         
         #xi_C_tot = parse_pdf(fit_lines[381:401])
         #t = parse_percentile(fit_lines[402])
-        t = parse_percentile(fit_lines[482])
+        #t = parse_percentile(fit_lines[482])
+        t = calc_percentiles(fit_lines[461:481])
         xi_C_tot_med.append(float(t[0]))
         xi_C_tot_percent.append(np.array(t[1:]))
 
         # PAH
         #t = parse_percentile(fit_lines[425])
-        t = parse_percentile(fit_lines[505])
+        #t = parse_percentile(fit_lines[505])
+        t = calc_percentiles(fit_lines[484:504])
         xi_PAH_tot_med.append(float(t[0]))
         xi_PAH_tot_percent.append(np.array(t[1:]))
 
         # MIR - hadn't been tracking these...
         #t = parse_percentile(fit_lines[448])
-        t = parse_percentile(fit_lines[528])
+        #t = parse_percentile(fit_lines[528])
+        t = calc_percentiles(fit_lines[507:527])
         xi_MIR_tot_med.append(float(t[0]))
         xi_MIR_tot_percent.append(np.array(t[1:]))
         
         #xi_W_tot = parse_pdf(fit_lines[450:470])
         #t = parse_percentile(fit_lines[471])
-        t = parse_percentile(fit_lines[551])
+        #t = parse_percentile(fit_lines[551])
+        t = calc_percentiles(fit_lines[530:550])        
         xi_W_tot_med.append(float(t[0]))
         xi_W_tot_percent.append(np.array(t[1:]))
         
         #tau_V_ISM = parse_pdf(fit_lines[473:553])
         #t = parse_percentile(fit_lines[554])
-        t = parse_percentile(fit_lines[634])
+        #t = parse_percentile(fit_lines[634])
+        t = calc_percentiles(fit_lines[553:633])        
         tau_V_ISM_med.append(float(t[0]))
         tau_V_ISM_percent.append(np.array(t[1:]))
         
         #Mdust = parse_pdf(fit_lines[556:616])                
         #t = parse_percentile(fit_lines[617])
-        t = parse_percentile(fit_lines[697])
+        #t = parse_percentile(fit_lines[697])
+        t = calc_percentiles(fit_lines[636:696])
         Mdust_med.append(float(t[0]))
         Mdust_percent.append(np.array(t[1:]))
 
         #SFR = parse_pdf(fit_lines[619:679]) 
         #t = parse_percentile(fit_lines[680])
         t = parse_percentile(fit_lines[810])
+        if args.verbose:
+            print("\nSFR parsing percentile line: ",t)
+        t = calc_percentiles(fit_lines[699:809],plotflag=True)
+        if args.verbose:
+            print("SFR calculating percentile line: ",t)        
         sfr_med.append(float(t[0]))
         sfr_percent.append(np.array(t[1:]))
 
